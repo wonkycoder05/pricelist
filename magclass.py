e99fa47c342ad00cc383d9e081a3ddb54e98c556
magdalena aretha <144786416+wonkycoder05@users.noreply.github.com>
2023-12-04 14:54:32 +0700

    Add files via upload

---
+++ magclass.py
@@ -0,0 +1,10 @@
+class Pricelist():
+    def __init__(self, name, amount):
+        self.__name = name
+        self.__amount = amount
+        self.__price = 0
+        self.__total = 0
+    def PriceList(self):
+        foodname = self.__name
+        if foodname : 'Dry Cured Iberian Ham' 
+        self.__price = 177.80
