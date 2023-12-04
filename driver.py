8c86f1fd5f650caa1f1f2a449998dd458d4a173a
magdalena aretha <144786416+wonkycoder05@users.noreply.github.com>
2023-12-04 16:20:47 +0700

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,2 +1,36 @@
 from magclass import Pricelist
+from magclass import TotalPrice
 
+print("The food in stock are: \n 1. Dry Cured Iberian Ham \n 2. Wagyoo Steaks \n 3. Matsutake Mooshrooms \n 4. Luwak Coffee \n 5. Moose Cheese \n 6. White Truffles \n 7. Bluefin Tuna \n 8. Le Bonnotte Potatoos")
+
+def CreateList():
+    ItemNum = int(input("How many do you want to order? Input here: "))
+    while ItemNum < 1 :
+        print("uh, your order has to be more than zero? input again here: ")
+     
+
+
+
+    for i in range (ItemNum):
+        itemList = []
+        print("Item #", i)
+        food = input("Enter Food:")
+        amt = float(input("Enter pounds:"))
+        
+        while amt<0 :
+            print("Oops, cannot.")
+
+        MyFood = Pricelist
+        itemList.append(MyFood)
+        return itemList
+    
+def displayList(foodList):
+    print("Here's the items purchased:")
+    print("-----------------------------------------")
+
+    for food in foodList:
+        foodList[food].__str__()
+
+def main():
+    myFoodlist = CreateList()
+    displayList(myFoodlist)        
--- magclass.py
+++ magclass.py
@@ -1,10 +1,41 @@
 class Pricelist():
+    __totalCost: 0.0
     def __init__(self, name, amount):
         self.__name = name
         self.__amount = amount
         self.__price = 0
         self.__total = 0
-    def PriceList(self):
-        foodname = self.__name
-        if foodname : 'Dry Cured Iberian Ham' 
+def PriceList(self):
+    foodname = self.__name
+    if foodname == 'Dry Cured Iberian Ham':
         self.__price = 177.80
+        return self.__price
+    elif foodname == 'Wagyu Steaks':
+        self.__price = 450.00
+        return self.__price
+    elif foodname == 'Matsutake Mooshrooms':
+        self.__price = 272.00
+        return self.__price
+    elif foodname == 'Moose Cheese':
+        self.__price = 487.20
+        return self.__price
+    elif foodname == 'White Truffles': 
+        self.__price = 3600.00
+        return self.__price
+    elif foodname == 'Bluefin Tuna':
+        self.__price = 3603.00
+        return self.__price
+    elif foodname == 'Le Bonnotte Potatoos':
+        self.__price = 270.81
+        return self.__price
+    else:
+        print('Invalid Food Name')
+class TotalPrice():
+    def __init__(self, price):
+        self.__price = price
+    def total_cost(self):
+        self.__total = self.__price * self.__amount 
+        return self.__total
+        
+def __str__(self):
+    return f"the total cost for {self.__amount} pounds of {self.__name} is {self.__total}"
